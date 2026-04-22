4ddc474fef04699ff4105c6d978b6ccc84362ba1
DEV <dev@dynamic-176-000-002-164.176.0.pool.telefonica.de>
2026-04-23 03:01:40 +0700

    Fix: Completely remove legacy OAuth routes and atob dependency

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -3,7 +3,6 @@ import express from "express";
 import { createServer } from "http";
 import net from "net";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { registerOAuthRoutes } from "./oauth";
 import { registerStorageProxy } from "./storageProxy";
 import { appRouter } from "../routers";
 import { createContext } from "./context";
@@ -17,7 +16,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 registerStorageProxy(app);
-registerOAuthRoutes(app);
 
 // tRPC API
 app.use(
